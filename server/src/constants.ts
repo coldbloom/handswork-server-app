3ae2dd59bc568b783974e5e6009ddfa1dcb66db4
konstantin.kolesnik <Konstan.Kolesnik@x5.ru>
2025-05-30 00:11:50 +0300

    edit cookie settings fix lax

--- server/src/constants.ts
+++ server/src/constants.ts
@@ -2,7 +2,7 @@ export const COOKIE_SETTINGS = {
   REFRESH_TOKEN: {
     httpOnly: true, // чтобы данная cookie не была доступна из js кода
     secure: false,  // @TODO изменить на true в production. true - Отправка только по HTTPS
-    sameSite: 'Lax',
+    // sameSite: 'lax', // значение lax должно быть по умолчанию
     maxAge: 1296e6, // 15 * 24 * 3600 * 1000 (15 дней)
   },
 };
